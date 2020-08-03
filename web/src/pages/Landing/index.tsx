77b95a28174f5065c8c01034de2c1e15c0bf6ec7
Ptdavid0 <pedroantonio.dj.pereira@gmail.com>
2020-08-03 18:18:18 -0300

    Landing page css done

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import logo from "../../assets/images/logo.svg";
+import landingImg from "../../assets/images/landing.svg";
+import studyicon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHearthIcon from "../../assets/images/icons/purple-heart.svg";
+
+import './styles.css'
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+
+        <div className="logo-container">
+          <img src={logo} alt="Proffy logo" />
+          <h2>The best plataform for online studies</h2>
+        </div>
+
+        <img
+          src={landingImg}
+          alt="Plataforma de estudos"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <a href="www.google.com" className="study">
+            <img src={studyicon} alt="study icon" />
+            Study
+          </a>
+
+          <a href="www.google.com" className="give-classes">
+            <img src={giveClassesIcon} alt="give classes icon" />
+            Give Classes
+          </a>
+        </div>
+
+        <span className="total-connections">
+          More than 200 connections already made
+          <img src={purpleHearthIcon} alt="Heart" />
+        </span>
+
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
