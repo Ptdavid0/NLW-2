5da0f9c34d5d3c9dc44218933be410d74ca3874f
Ptdavid0 <pedroantonio.dj.pereira@gmail.com>
2020-08-04 18:28:27 -0300

    Server ready/ End of day two

---
+++ server/src/routes.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import db from "./database/connection";
+import ClassesController from "./controllers/ClassesController";
+import ConnectionsController from "./controllers/ConnectionsController";
+
+const routes = express.Router();
+
+const classesController = new ClassesController()
+const connectionController = new ConnectionsController();
+
+routes.post("/classes", classesController.create );
+routes.get("/classes", classesController.index );
+
+routes.post("/connections", connectionController.create );
+routes.get("/connections", connectionController.index );
+
+
+
+
+export default routes;
